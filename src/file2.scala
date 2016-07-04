535c3f509782953e81834b1b13038d4a3026878c
Yaniv Rodenski <roadan@gmail.com>
2016-07-04 22:29:58 +1000

    added println

--- src/file2.scala
+++ src/file2.scala
@@ -1,5 +1,6 @@
 import io.shinto.amaterasu.execution.AmaContext
 
+println("@@@@@@@@@@@@@@@@@@@@@@@@@")
 val oddRdd = AmaContext.getRDD[Int]("start", "rdd").filter(x=>x/2 == 0)
 oddRdd.take(5).foreach(println)
 
