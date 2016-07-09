1098220b80ee366380a270cebb713491c7b958b6
Yaniv Rodenski <roadan@gmail.com>
2016-07-09 16:59:28 +1000

    fixed first action

--- src/file.scala
+++ src/file.scala
@@ -1,7 +1,5 @@
-println("@@@@@@@@@@@@@@@1@@@@@@@@@@@@@@@")
-ta = Array(1, 2, 3, 4, 5)
+val data = Array(1, 2, 3, 4, 5)
 val x = data.tail
 
 val rdd = sc.parallelize(data)
 val odd = rdd.filter(n => n%2 != 0)
-println("@@@@@@@@@@@@@@2@@@@@@@@@@@@@@@")
