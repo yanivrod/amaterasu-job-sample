9b67ceb24657067fe15b8e972786c9fc8dab5d95
Yaniv Rodenski <roadan@gmail.com>
2016-07-13 18:47:20 +1000

    added saving a df

--- src/file2.scala
+++ src/file2.scala
@@ -1,8 +1,7 @@
 import io.shinto.amaterasu.execution.AmaContext
 
-println("@@@@@@@@@@@@@@@@@@@@@@@@@")
 val oddRdd = AmaContext.getRDD[Int]("start", "rdd").filter(x=>x/2 == 0)
 oddRdd.take(5).foreach(println)
 
 val highNoDf = AmaContext.getDataFrame("start", "x").where("_1 > 3")
-highNoDf.show
+highNoDf.write.json("file:///tmp/test1")
