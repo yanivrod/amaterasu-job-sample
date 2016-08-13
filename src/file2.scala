04576e172de330409b32a231411d7d4e380afdde
Yaniv Rodenski <roadan@gmail.com>
2016-08-13 18:40:23 +1000

    using env for output

--- src/file2.scala
+++ src/file2.scala
@@ -4,4 +4,4 @@ val oddRdd = AmaContext.getRDD[Int]("start", "rdd").filter(x=>x/2 == 0)
 oddRdd.take(5).foreach(println)
 
 val highNoDf = AmaContext.getDataFrame("start", "x").where("_1 > 3")
-highNoDf.write.json("file:///tmp/test1")
+highNoDf.write.json(s"${env.outputRootPath}/test1")
