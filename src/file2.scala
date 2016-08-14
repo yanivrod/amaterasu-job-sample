533c6efa5585cc76669698ecbeeb804df3382550
Yaniv Rodenski <roadan@gmail.com>
2016-08-14 19:26:29 +1000

    fixd job error

--- src/file2.scala
+++ src/file2.scala
@@ -1,5 +1,5 @@
-val oddRdd = AmaContext.getRDD[Int]("start", "rdd").filter(x=>x/2 == 0)
+val oddRdd = AmaContext.getRDD[Int]("start", "rdd").filter(x=>x%2 == 0)
 oddRdd.saveAsTextFile(s"${env.outputRootPath}/test/ordd_rdd")
 
-val highNoDf = AmaContext.getDataFrame("start", "x").where("_1 > 3")
+val highNoDf = AmaContext.getDataFrame("start", "odd").where("_1 > 100")
 highNoDf.write.json(s"${env.outputRootPath}/dtatframe_res")
