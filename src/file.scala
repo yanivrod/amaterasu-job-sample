74e5cfa2bec05ce8d698e8538b941d3526dd5b8e
Yaniv Rodenski <roadan@gmail.com>
2016-08-14 19:47:54 +1000

    added a dataframe

--- src/file.scala
+++ src/file.scala
@@ -2,4 +2,6 @@ val data = 1 to 1000
 val x = data.tail
 
 val rdd = sc.parallelize(data)
-val odd = rdd.filter(n => n%2 != 0)
+
+import sqlContext.implicits._
+val odd = rdd.filter(n => n%2 != 0).toDF("number")
--- src/file2.scala
+++ src/file2.scala
@@ -1,5 +1,5 @@
 val oddRdd = AmaContext.getRDD[Int]("start", "rdd").filter(x=>x%2 == 0)
 oddRdd.saveAsTextFile(s"${env.outputRootPath}/test/ordd_rdd")
 
-val highNoDf = AmaContext.getDataFrame("start", "odd").where("_1 > 100")
+val highNoDf = AmaContext.getDataFrame("start", "odd").where("number > 100")
 highNoDf.write.json(s"${env.outputRootPath}/dtatframe_res")
