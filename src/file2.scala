754d101733e91b164cba1067a74eefb274cfd364
Yaniv Rodenski <roadan@gmail.com>
2016-08-14 15:16:21 +1000

    fixd take issue

--- src/file2.scala
+++ src/file2.scala
@@ -1,5 +1,5 @@
 val oddRdd = AmaContext.getRDD[Int]("start", "rdd").filter(x=>x/2 == 0)
-oddRdd.take(5).saveAsTextFile(s"${env.outputRootPath}/test/ordd_rdd")
+oddRdd.saveAsTextFile(s"${env.outputRootPath}/test/ordd_rdd")
 
-//val highNoDf = AmaContext.getDataFrame("start", "x").where("_1 > 3")
-//highNoDf.write.json(s"${env.outputRootPath}/test1")
+val highNoDf = AmaContext.getDataFrame("start", "x").where("_1 > 3")
+highNoDf.write.json(s"${env.outputRootPath}/dtatframe_res")
