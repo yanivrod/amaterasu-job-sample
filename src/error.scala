be6536d6c2af5f740c7e01cfd420713ea724b09d
Yaniv Rodenski <roadan@gmail.com>
2016-08-28 22:00:16 +1000

    fixed token

--- src/error.scala
+++ src/error.scala
@@ -1,5 +1,5 @@
 import com.flyberrycapital.slack.SlackClient
 
-val s = new SlackClient("xoxp-73743425940-73697828947-73755088949-dc73460073i")
+val s = new SlackClient("xoxp-73743425940-73697828947-73755088949-dc73460073")
 
 s.chat.postMessage("#demos", "Hello World!")
