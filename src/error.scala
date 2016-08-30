795d69d8f41e8005259de9da3c20f97f6761b9e8
Yaniv Rodenski <roadan@gmail.com>
2016-08-30 14:25:25 +1000

    aded err msg config

--- src/error.scala
+++ src/error.scala
@@ -2,4 +2,4 @@ import com.flyberrycapital.slack.SlackClient
 
 val s = new SlackClient("xoxp-73743425940-73697828947-73755088949-dc73460073")
 
-s.chat.postMessage("#demos", "To make error is human. To propagate error to all server in automatic way is #devops. \n              -DevOps Borat")
+s.chat.postMessage("#demos", env.configuration("err-msg" ))
