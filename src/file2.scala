7ff87b80ded2499d755b674d998fca6318f3e1d4
Yaniv Rodenski <roadan@gmail.com>
2017-05-17 22:16:30 +1000

    Update file2.scala

--- src/file2.scala
+++ src/file2.scala
@@ -1,4 +1,4 @@
-val oddRdd = AmaContext.getRDD[Int]("start", "rdd").filter(x=>x%2 == 0).map(x=>x*1)
+val oddRdd = AmaContext.getRDD[Int]("start", "rdd").filter(x=>x%2 == 0)
 oddRdd.saveAsTextFile(s"${env.outputRootPath}/test/ordd_rdd")
 
 val highNoDf = AmaContext.getDataFrame("start", "odd").where("number > 100")
