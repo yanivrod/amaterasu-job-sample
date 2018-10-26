a54e54102966fd8fd6df1d87d7382a58d308c2c6
Yaniv Rodenski <roadan@gmail.com>
2018-10-26 18:39:10 +1100

    Update file.scala

--- src/file.scala
+++ src/file.scala
@@ -1,5 +1,7 @@
-val data = 1 to 1000 
+class main extends App {
+  val data = 1 to 1000 
 
-val rdd = sc.parallelize(data)
+  val rdd = sc.parallelize(data)
 
-val odd = rdd.filter(n => n%2 != 0).toDF("number")
+  val odd = rdd.filter(n => n%2 != 0).toDF("number")
+}
